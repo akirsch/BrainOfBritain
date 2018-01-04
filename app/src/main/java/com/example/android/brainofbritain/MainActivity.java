04de40773ef9c6e0a4366dab605dd60ee92d9aa2
amk87 <adamkirsch1@gmail.com>
2018-01-04 22:34:57 +0200

    feat: improved java code

--- app/src/main/java/com/example/android/brainofbritain/MainActivity.java
+++ app/src/main/java/com/example/android/brainofbritain/MainActivity.java
@@ -9,38 +9,82 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
+    static final String STATE_USERSCORE = "score";
+    static final String STATE_RESULTS_MESSAGE = "resultsMessage";
+
     int score;
-    boolean isAnsweredCorrectlyQ1 = false;
-    boolean isAnsweredCorrectlyQ2 = false;
-    boolean isAnsweredCorrectlyQ3 = false;
-    boolean isAnsweredCorrectlyQ4 = false;
-    boolean isAnsweredCorrectlyQ5 = false;
-    boolean isAnsweredCorrectlyQ6 = false;
-    boolean isAnsweredCorrectlyQ7 = false;
-    boolean isAnsweredCorrectlyQ8 = false;
-    boolean isAnsweredCorrectlyQ9 = false;
-    boolean isAnsweredCorrectlyQ10 = false;
+    String resultsMessage;
+
+    CheckBox checkbox1;
+    CheckBox checkbox2;
+    CheckBox checkbox3;
+    CheckBox checkbox4;
+    TextView resultsView;
+    EditText userName;
+
+    // create array to store View Id'd of correct answers
+    RadioButton correctAnswers[] = new RadioButton[9];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        correctAnswers[0] = (findViewById(R.id.q1_a2));
+        correctAnswers[1] = (findViewById(R.id.q2_a1));
+        correctAnswers[2] = (findViewById(R.id.q3_a4));
+        correctAnswers[3] = (findViewById(R.id.q4_a3));
+        correctAnswers[4] = (findViewById(R.id.q5_a2));
+        correctAnswers[5] = (findViewById(R.id.q7_a3));
+        correctAnswers[6] = (findViewById(R.id.q6_a3));
+        correctAnswers[7] = (findViewById(R.id.q8_a2));
+        correctAnswers[8] = (findViewById(R.id.q10_a1));
+
+        checkbox1 = findViewById(R.id.q9_checkBox1);
+        checkbox2 = findViewById(R.id.q9_checkBox2);
+        checkbox3 = findViewById(R.id.q9_checkBox3);
+        checkbox4 = findViewById(R.id.q9_checkBox4);
+        resultsView = findViewById(R.id.result_view);
+        userName = findViewById(R.id.enter_name_view);
+        resultsMessage = getString(R.string.initial_result_message);
+
+        resultsView.setText(resultsMessage);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        // Save the user's current score state
+        savedInstanceState.putInt(STATE_USERSCORE, score);
+        savedInstanceState.putString(STATE_RESULTS_MESSAGE, resultsMessage);
+
+        // Always call the superclass so it can save the view hierarchy state
+        super.onSaveInstanceState(savedInstanceState);
+
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        // Always call the superclass so it can restore the view hierarchy
+        super.onRestoreInstanceState(savedInstanceState);
+
+        // Restore state members from saved instance
+        score = savedInstanceState.getInt(STATE_USERSCORE);
+        resultsMessage = savedInstanceState.getString(STATE_RESULTS_MESSAGE);
     }
 
     /**
-     * This method checks whether the correct answer has been selected by the user, and updates the
-     * score accordingly.
-     *
-     * @param viewId the id of a radio button
+     * This method checks whether the correct Radio Button answers have been selected by the user,
+     * and updates the score accordingly.
      */
-    public void checkCorrectAnswer (int viewId, boolean isAnsweredCorrectly) {
-        RadioButton radioButton = findViewById(viewId);
-        if (radioButton.isChecked()) {
-            if (!isAnsweredCorrectly) {
-                score ++;
-                isAnsweredCorrectly = true;
+    public void checkCorrectRadioButtonAnswers() {
+        for (int i = 0; i < 9; i++) {
+            RadioButton radioButton = correctAnswers[0];
+            if (radioButton.isChecked()) {
+                score++;
             }
         }
     }
@@ -51,17 +95,10 @@ public class MainActivity extends AppCompatActivity {
      */
 
     public void checkCorrectCheckBoxAnswer() {
-        if (!isAnsweredCorrectlyQ9) {
-            CheckBox checkbox1 =  findViewById(R.id.q9_checkBox1);
-            CheckBox checkbox2 =  findViewById(R.id.q9_checkBox2);
-            CheckBox checkbox3 =  findViewById(R.id.q9_checkBox3);
-            CheckBox checkbox4 =  findViewById(R.id.q9_checkBox4);
+        if ((checkbox1.isChecked() && checkbox3.isChecked())
+                && (!checkbox2.isChecked() && !checkbox4.isChecked())) {
+            score++;
 
-            if ((checkbox1.isChecked() && checkbox3.isChecked())
-                    && (!checkbox2.isChecked() && !checkbox4.isChecked())) {
-                score++;
-                isAnsweredCorrectlyQ9 = true;
-            }
         }
     }
 
@@ -73,26 +110,10 @@ public class MainActivity extends AppCompatActivity {
      */
     private int calculateScore() {
         score = 0;
-        // check if user selected correct answer to Q1
-        checkCorrectAnswer(R.id.q1_a2, isAnsweredCorrectlyQ1);
-        // check if user selected correct answer to Q2
-        checkCorrectAnswer(R.id.q2_a1, isAnsweredCorrectlyQ2);
-        // check if user selected correct answer to Q3
-        checkCorrectAnswer(R.id.q3_a4, isAnsweredCorrectlyQ3);
-        // check if user selected correct answer to Q4
-        checkCorrectAnswer(R.id.q4_a3, isAnsweredCorrectlyQ4);
-        // check if user selected correct answer to Q5
-        checkCorrectAnswer(R.id.q5_a2, isAnsweredCorrectlyQ5);
-        // check if user selected correct answer to Q6
-        checkCorrectAnswer(R.id.q6_a3, isAnsweredCorrectlyQ6);
-        // check if user selected correct answer to Q7
-        checkCorrectAnswer(R.id.q7_a3, isAnsweredCorrectlyQ7);
-        // check if user selected correct answer to Q8
-        checkCorrectAnswer(R.id.q8_a2, isAnsweredCorrectlyQ8);
+        // check if user selected correct answer to RadioButton Questions
+        checkCorrectRadioButtonAnswers();
         // check if user selected correct answer to Q9
-       checkCorrectCheckBoxAnswer();
-        // check if user selected correct answer to Q10
-        checkCorrectAnswer(R.id.q10_a1, isAnsweredCorrectlyQ10);
+        checkCorrectCheckBoxAnswer();
 
         return score;
     }
@@ -100,15 +121,13 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This method is called when the submit answers button is clicked.
      */
-    public void displayResult (View view){
-        TextView textView = findViewById(R.id.result_view);
+    public void displayResult(View view) {
         int result = calculateScore();
-        EditText enterName = (EditText) findViewById(R.id.enter_name_view);
-        String name = enterName.getText().toString();
-        String resultMessage = name
-                + getResources().getString(R.string.resultMessagePart1) + " " +
+        String name = userName.getText().toString();
+        resultsMessage = name
+                + getResources().getString(R.string.resultMessagePart1) + " "
                 + result + " " + getResources().getString(R.string.resultMessagePart2);
-        textView.setText(resultMessage);
+        resultsView.setText(resultsMessage);
     }
 
     /**
@@ -116,34 +135,27 @@ public class MainActivity extends AppCompatActivity {
      *
      * @param radioGroupID the id of a radio group
      */
-    private void resetQuestions (int radioGroupID) {
+    private void resetQuestions(int radioGroupID) {
         RadioGroup radioGroup = findViewById(radioGroupID);
         radioGroup.clearCheck();
     }
 
     /**
      * This method resets all the CheckBoxes in the quiz.
-     *
      */
 
-    private void resetCheckBoxes(){
-        CheckBox checkbox1 = findViewById(R.id.q9_checkBox1);
-        CheckBox checkbox2 = findViewById(R.id.q9_checkBox2);
-        CheckBox checkbox3 = findViewById(R.id.q9_checkBox3);
-        CheckBox checkbox4 = findViewById(R.id.q9_checkBox4);
-
+    private void resetCheckBoxes() {
         checkbox1.setChecked(false);
         checkbox2.setChecked(false);
         checkbox3.setChecked(false);
         checkbox4.setChecked(false);
-
     }
 
     /**
      * This method is called when the reset quiz button is clicked. Method resets all Radio Buttons
      * and resets score to 0.
      */
-    public void resetQuiz (View view) {
+    public void resetQuiz(View view) {
         resetQuestions(R.id.radioGroupQ1);
         resetQuestions(R.id.radioGroupQ2);
         resetQuestions(R.id.radioGroupQ3);
@@ -157,10 +169,8 @@ public class MainActivity extends AppCompatActivity {
 
         score = 0;
 
-        TextView textView = findViewById(R.id.result_view);
-        textView.setText(getString(R.string.initial_result_message));
-        EditText editText = findViewById(R.id.enter_name_view);
-        editText.setText(getString(R.string.user_name));
+        resultsView.setText(getString(R.string.initial_result_message));
+        userName.setText(getString(R.string.user_name));
     }
 
 }
